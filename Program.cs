b632030c07e78e37702b39c810ef8a8498d7fe14
Jacob Clark <jacobclark08@gmail.com>
2021-07-14 15:12:10 -0400

    Removed unneeded Class files, added Genders list

--- Program.cs
+++ Program.cs
@@ -1,5 +1,7 @@
 ﻿using System.Collections.Generic;
 using System;
+using System.IO;
+
 
 namespace dndRandoGen
 {
@@ -7,7 +9,19 @@ namespace dndRandoGen
     {
         public static void Main(string[] args)
         {
-
+            List<string> races = new() { "Orc", "Human", "Elf", "Gnome", "Halfling" };
+            List<string> classes = new() { "Warrior", "Bard", "Wizard", "Cleric", "Rogue" };
+            List<string> genders = new() { "Male", "Female", "Non-Binary", "Trans-Masc", "Trans-Femme" };
         }
+        private static string Randomize(List<string> list)
+        {
+            Random random = new();
+            int index = random.Next(list.Count);
+            string rand = list[index];
+            return rand;
+        }
+
+        private static 
+
     }
 }
