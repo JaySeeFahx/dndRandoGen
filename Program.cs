108d18f22e10dfca63d88e172f93c9ca11e5fa4d
Jacob Clark <jacobclark08@gmail.com>
2021-07-14 13:18:40 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using System;
+
+namespace dndRandoGen
+{
+    public static class Program
+    {
+        public static void Main(string[] args)
+        {
+
+        }
+    }
+}
---
+++ Race.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dndRandoGen
+{
+    class Race
+    {
+        List<string> races = new() { "Orc", "Human", "Elf", "Gnome", "Halfling" };
+    }
+}
