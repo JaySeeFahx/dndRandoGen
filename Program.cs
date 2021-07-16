b0ce8532b66506aa310375acd05d68f0ee0bb2d1
Jacob Clark <jacobclark08@gmail.com>
2021-07-16 14:00:58 -0400

    Added try/catch statement to main menu

--- Program.cs
+++ Program.cs
@@ -7,6 +7,8 @@ namespace dndRandoGen
 {
     public static class Program
     {
+        static NewCharacter NewToon = new();
+        
         public static void Main(string[] args)
         {
             bool showMenu = true;
@@ -16,8 +18,7 @@ namespace dndRandoGen
             }
         }
         private static bool MainMenu()
-        {
-            NewCharacter nc = new();
+    {
             Console.Clear();
             Console.WriteLine("Welcome Adventurer!");
             Console.WriteLine("Please choose an option:");
@@ -25,20 +26,37 @@ namespace dndRandoGen
             Console.WriteLine("2) View Saved Character");
             Console.WriteLine("3) Exit");
             Console.Write("Select an option:");
-
-            switch (Console.ReadLine())
+            try
             {
-                case "1":
-                    nc.NewChar();
-                    return true;
-                case "2":
-                    //SavedChar();
-                    return true;
-                case "3":
-                    return false;
-                default:
-                    return false;
+                string input = Console.ReadLine();
+                int switchCall = int.Parse(input);
+
+                switch (switchCall)
+                {
+                    case 1:
+                        Console.Clear();
+                        NewToon.NewChar();
+                        return true;
+                    case 2:
+                        Console.Clear();
+                        //SavedChar();
+                        return true;
+                    case 3:
+                        Console.Clear();
+                        Console.WriteLine("It's dangerous to go alone. Thanks for finding your new party member(s) with us!\n");
+                        return false;
+                    default:
+                        Console.Clear();
+                        Console.WriteLine("Please choose carefully! The option you chose isn't valid. Press any key to return to the main menu!");
+                        Console.ReadKey();
+                        return true;
+                }
             }
+            catch (FormatException)
+            {
+                throw;
+            }
+
         }
     }
 }
